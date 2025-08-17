b16bd0c2932efc79c6c4c69c339c6bcacf9969f7
jcahet <ygorcahet@gmail.com>
2025-08-16 22:56:39 -0300

    código do desafio

--- desafio-felipao/index.js
+++ desafio-felipao/index.js
@@ -1,1 +1,32 @@
 // iniciando
+let nomeHeroi = "Bravus";
+let xp = 2000;
+
+if (xp <= 1000 ) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Ferro`);    
+    
+}else if (xp <= 2000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Bronze`);
+    
+}else if (xp <= 5000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Prata`);
+        
+}else if (xp <= 7000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Ouro`);
+        
+}else if (xp <= 8000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Platina`);
+    
+}else if (xp <= 9000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Ascendente`);
+    
+}else if (xp <= 10000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Imortal`);
+    
+}else if (xp <= 11000) {
+    console.log(`XP:[${xp}] | ${nomeHeroi} seu nível é: Radiante`);
+    
+}else{
+    console.log(`Visualização de status apartir do nível "Ferro"`);
+    
+}
